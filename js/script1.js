e5bbb7e2368c56a6e6fbf7a6a7b21909949c9b52
YoryieSkvar00 <jorgeescobar3456@gmail.com>
2023-09-30 21:40:46 -0400

    parte3

---
+++ js/script1.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Oculta la pantalla de carga cuando el contenido se ha cargado
+    var loader = document.getElementById("loader");
+    loader.style.display = "none";
+});
